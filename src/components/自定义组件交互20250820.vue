d6e6ee900a741747f8204c61b54f92a43021b3f4
hoshinochen <2909284990@qq.com>
2025-08-20 11:11:31 +0800

    自定义组件交互emit

---
+++ src/components/自定义组件交互20250820.vue
@@ -0,0 +1,23 @@
+﻿<template>
+    <h2>自定义组件交互20250820</h2>
+    <h3>hohsino</h3>
+    <button @click="trans">传递数据</button>
+</template>
+<script>
+    export default{
+        name:"emit",
+        data(){
+            return{
+                message:"数据传递成功！"
+            }
+        },
+        methods:{
+            trans(transContent){
+                //俩参数，第一个是名称，第二个是参数
+                this.$emit("transfer",this.message);
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
