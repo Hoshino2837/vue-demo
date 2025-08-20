285f7805cc2b862e5c8a098c26d2ee449f0716d1
hoshinochen <2909284990@qq.com>
2025-08-20 11:12:17 +0800

    app增加自定义组件交互

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,40 @@
   <!-- <forms/> -->
   <!-- <process/> -->
   <!-- <model/> -->
-   <props :message="message" :age="age" :name="name"/>
+  <!-- <props :message="message" :age="age" :name="name"/> -->
+  <emit @transfer="transferMessage"/>
 </template>
 
 <script>
   // import HelloWorld from './components/HelloWorld.vue',
-  // import if20250818 from './components/条件渲染20250818.vue'
-  // import forms from './components/forms20250819.vue'
-  // import process from './components/事件处理20250819.vue'
-  // import model from './components/表单输入绑定20250819.vue'
-  import props from './components/props20250819.vue'
+  // import if20250818 from './components/条件渲染20250818.vue',
+  // import forms from './components/forms20250819.vue',
+  // import process from './components/事件处理20250819.vue',
+  // import model from './components/表单输入绑定20250819.vue',
+  // import props from './components/props20250819.vue',
+  import emit from './components/自定义组件交互20250820.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    // if20250818
-    // forms
-    // process
-    // model
-    props
+    // if20250818,
+    // forms,
+    // proces,s
+    // model,
+    // props,
+    emit,
   },
   data(){
     return{
     message:"hello",
     age:30,
     name:["hoshino", "tsukino"],
+    }
+  },
+  methods:{
+    transferMessage(transContent){
+      alert(transContent);
     }
   }
 }
